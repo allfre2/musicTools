7d4d475884be335abf6e1e9f6ead881ceb8e41c2
allfre2 <allfre2luzon@gmail.com>
2018-06-09 22:04:30 -0400

    ...

--- src/main/java/com/allfre2/musicruler/songs/main.java
+++ src/main/java/com/allfre2/musicruler/songs/main.java
@@ -37,14 +37,21 @@ public class main{
 
   String test4 = "poem = verse-1-23-23 nl verse-9-8-7 nl verse-2-23-5;";
 
-  String test5 =  "verse = prog;"
-                 +"prog = (T | t | D | SD ) (T | t | SD ) (D | t | SD) T;"
+  String test5 =  "verse = progMaj | progMin;"//| progMod;"
+                 +"progMaj = (T | t | D | SD ) (T | t | SD ) (D | t | SD) T;"
                  +"T = I | Imaj7;"
-                 +"T = i | i-7;"
-                 +"t = iii | iii-7 | vi | bVI | bIII | bIII+;"
-                 +"SD = IV | iv | ii | iio | ii-7b5 | ii-7;"
-                 +"D = V | V7 | viio | viio7 | v | v-7;"
-                 +"n = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;";
+                 +"t = iii | iii-7 | vi | vi-7;"
+                 +"SD = IV | IVmaj7 | ii | ii-7;"
+                 +"D = V | V7 | viio | viio7;"
+                 +"progMin = (Tm | tm | Dm | SDm ) (Tm | tm | SDm ) (Dm | tm | SDm) Tm;"
+                 +"Tm = i | i-7;"
+                 +"tm = bIII | bIIImaj7 | bVI | bVImaj7;"
+                 +"SDm = iv | iv-7 | iio | ii-7b5;"
+                 +"Dm = v | v-7 | bVII | bVIImaj7;"
+                 +"progMod = (T | Tm | t | tm | D | Dm | SD | SDm)"
+                           +"(T | Tm | t | tm | SD | SDm)"
+                           +"(D | Dm | t | tm | SD | SDm) (T | Tm);"
+                 ;
 
   try{
   // CFG cfg = new CFG("G = A B C; \n\n== A \n= B\n| C|G;\n\n\nF= (V)| (A| C);; =A;G  ");
@@ -58,19 +65,24 @@ public class main{
   System.out.println("Random: " + t3.genRandom("prog", 5));
 
   CFG t4 = new  CFG(test4);
-  System.out.println("Random: " + t4.genRandom("poem", 5));
+  System.out.println("random: " + t4.genRandom("poem", 5));
 
   CFG t5 = new CFG(test5);
-  for(int i = 0; i < 6; ++i){
-   System.out.println("Random: " + t5.genRandom("verse", 7));
-   io.input();
-  }
+  // for(;;){
+  //  system.out.println("random: " + t5.genRandom("verse", 7));
+  //  if(!io.input().isempty()) system.exit(0);
+  // }
 
-  Lyrics poem = new Limerick(new URLDataSource("https://www.gnu.org/licenses/gpl-3.0.txt"));
-  // Limerick poem = new Limerick(new FileDataSource("/home/allfre2/a.txt"));
-  System.out.println(poem.generate() + "\n");
-  System.out.println(poem.generate() + "\n");
-  System.out.println(poem.generate() + "\n");
+  // Lyrics poem = new Limerick(new URLDataSource("https://www.gnu.org/licenses/gpl-3.0.txt"));
+  Limerick poem = new Limerick(new FileDataSource("/home/allfre2/a.txt"));
+  String p = poem.generate();
+  for(String s: p.split("\n")){
+    System.out.println(t5.genRandom("verse",7));
+    System.out.println(s);
+  }
+  // System.out.println(poem.generate() + "\n");
+  // System.out.println(poem.generate() + "\n");
+  // System.out.println(poem.generate() + "\n");  
   }catch(InvalidCFGException e){
   	e.printStackTrace();
   }
