b714e58a31ec0103280a4f32a464b7f36eef300f
allfre2 <allfre2luzon@gmail.com>
2018-06-11 17:06:01 -0400

    ...

--- src/main/java/com/allfre2/musicruler/songs/dummy2.java
+++ src/main/java/com/allfre2/musicruler/songs/dummy2.java
@@ -1,22 +1,64 @@
 package com.allfre2.musicruler.songs;
 
 import com.allfre2.musicruler.tools.io;
+import java.util.Random;
 
 public class dummy2{
     
+ public static final Random rnd = new Random();
+
  public static final String[] cfgs = {
-   "verse = chord;"
-   +"chord = 1 | 2 | 3 | 4 | 5 | 6 | 7;"
-   +"1 = I  5 | 3 | 6 | 2 | 7 | 4;"
-   +"2 = (ii (5 | 4 | 6 | 7)) | (bII7 5 );"
-   +"3 = iii 6 | 5 | 1 | 7;"
-   +"4 = IV 5 | 7 | 2 | 6;"
-   +"5 = (V | V7 | bII7) 1 | 3 | 6 | 7;"
-   +"6 = vi 4 | 1 | 3 | 5;"
-   +"7 = (viio | bVII) 1 | 5 | 3;"
+ 	""
+ 	//Major
+   // +"major = W W W ;"
+   +"major = W W ;"
+   +"W = (t) (D | SD);"
+   +"W = (D | SD) (T | t);"
+   +"T = I;"
+   +"t = iii | vi;"
+   +"SD = IV | ii;"
+   +"D = V | V7 | viio;"
+   //Minor
+   // +"minor = w w w ;"
+   +"minor = w w ;"
+   +"w = (Tm | tm) (Dm | SDm);"
+   +"w = (Dm | SDm) (Tm| tm);"
+   +"Tm = i;"
+   +"tm = III | VI;"
+   +"SDm = iv | iio;"
+   +"Dm = v | VII;"
+   //Mixolydian
+   // +"mixo = Wx Wx Wx ;"
+   +"mixo = Wx Wx ;"
+   +"Wx = (T | tx) (Dm | SD);"
+   +"Wx = (Dm | SD) (tx);"
+   +"tx = iiio | vi;"
+   //Dorian
+   // +"dorian = Wd Wd Wd ;"
+   +"dorian = Wd Wd ;"
+   +"Wd = (Tm | td) (Dm | SD);"
+   +"Wd = (Dm | SD) (td);"
+   +"td = III | vio;"
+   //Frygian
+   // +"frygian = Wf Wf Wf ;"
+   +"frygian = Wf Wf ;"
+   +"Wf = (Tm | tm) (Df | SDf);"
+   +"Wf = (Df | SDf) (tm);"
+   +"SDf = iv | bII;"
+   +"Df = vo | vii;"
+   //Lydian
+   // +"lydian = Wl Wl Wl ;"
+   +"lydian = Wl Wl ;"
+   +"Wl = (T | t) (Dl | SDl);"
+   +"Wl = (Dl | SDl) ( t);"
+   +"SDl = #ivo | II;"
+   +"Dl = V | vii;"
  };
 
+ static final String[] modes = {"major", "minor"};//, "dorian", "frygian", "mixo", "lydian"};
+
  public static void main(String[] args){
+   Limerick poem = new Limerick(new FileDataSource("/home/allfre2/a.txt"));
  	CFG c = null;
   for(String cfg: cfgs){
   	try{
@@ -24,7 +66,14 @@ public class dummy2{
   	}catch(Exception e){ e. printStackTrace(); }
   	 c.printGrammar();
   	 for(;;){
-  	 	System.out.println(c.genRandom("verse", 8) + "\n\n");
+  	 	// System.out.println(c.genRandom("verse", 8) + "\n\n");
+       String p = poem.generate();
+       for(String s: p.split("\n")){
+       String mode = modes[rnd.nextInt(modes.length)];
+         System.out.println(c.genRandom("major",8));
+         // System.out.println(c.genRandom(mode,8));
+         System.out.println(s);
+       }
   	 	if(!io.input().isEmpty()) break;
   	 }
   }
